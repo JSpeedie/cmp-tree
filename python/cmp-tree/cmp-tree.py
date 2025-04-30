214ace568f79144382e19335968a33baa97a75b5
JSpeedie <julianspeedie@gmail.com>
2025-04-30 09:20:07 -0400

    Fixed bug in relative_files_in_trees, finished files_in_tree

--- python/cmp-tree/cmp-tree.py
+++ python/cmp-tree/cmp-tree.py
@@ -1,4 +1,5 @@
 import filecmp # Possibly unnecessary
+import os
 from pathlib import Path
 
 
@@ -27,26 +28,49 @@ def relative_files_in_tree(root: Path, extension: Path) -> [Path]:
     '''
     # {{{
     ret = []
-    dir_path = root.join(extension);
+    dir_path = root / extension
 
     # If we are able to open the directory successfully */
     for f in os.scandir(dir_path):
-            # Only includes files that are not the special "." and ".." entries
-            if f.name == "." and f.name == "..":
-                # fp = full path, rp = relative path
-                file_fp = dir_path.join(f.name)
-                file_rp = extension.join(f.name)
-                ret.append(file_rp)
+        # Only includes files that are not the special "." and ".." entries
+        if f.name != "." and f.name != "..":
+            # fp = full path, rp = relative path
+            file_fp = dir_path / f.name
+            file_rp = extension / f.name
+            ret.append(file_rp)
 
-                # If the current element is a directory...
-                if f.is_dir():
-                    # Recurse and append the sub directory relative file paths
-                    sub_dir_files = relative_files_in_tree(root, file_rp);
-                    ret.extend(sub_dir_files);
+            # If the current element is a directory...
+            if f.is_dir():
+                # Recurse and append the sub directory relative file paths
+                sub_dir_files = relative_files_in_tree(root, file_rp)
+                ret.extend(sub_dir_files)
 
     return ret
     # }}}
 
 
+def files_in_tree(root: Path) -> [Path]:
+    '''
+    Returns an unsorted vector list of file paths for all the files (in the
+    broad sense of the word, including links and directories, as well as hidden
+    files) in a directory tree rooted at the directory pointed to by `root`
+
+    Args:
+        `root`: a file path to the directory that roots a directory tree for
+            which we wish to get a list of all the files (in the broad sense)
+            contained within.
+
+    Returns:
+        An unsorted list of the relative file paths for all the files (in the
+        broad sense) in the directory tree rooted at `root`.
+    '''
+    # {{{
+    extension = Path("")
+    return relative_files_in_tree(root, extension)
+    # }}}
+
+
+
 if __name__ == "__main__":
-    print("code still in progress. Check back later")
+    # print(files_in_tree(Path("~/kingston1")))
+    print(files_in_tree(Path("/home/me/kingston1")))
