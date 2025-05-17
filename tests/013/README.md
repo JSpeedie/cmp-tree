e74261ea27a2f65efefae5be406ddff83fe34dc3
JSpeedie <julianspeedie@gmail.com>
2025-05-17 19:28:07 -0400

    Added test 13 which takes the form of a script which produces the directory tree and its contents

---
+++ tests/013/README.md
@@ -0,0 +1,54 @@
+# Test Input 013
+
+This test input has 2 almost-identical directory trees with multiple nested
+directories, each with one file contained within. The files in the directory
+trees are fully identical in terms of existence, file-type, substance, and
+modification time, except for `[...]/02/03/04/05/06/gradients.mov` which
+differs in substance and modification time.
+
+```
+first                                                                second
+├── 02                                                               ├── 02
+│   ├── 03                                                           │   ├── 03
+│   │   ├── 04                                                       │   │   ├── 04
+│   │   │   ├── 05                                                   │   │   │   ├── 05
+│   │   │   │   ├── 06                                               │   │   │   │   ├── 06
+│   │   │   │   │   └── gradients.mov  <-- differs in substance -->  │   │   │   │   │   └── gradients.mov
+│   │   │   │   └── test.mp4                                         │   │   │   │   └── test.mp4
+│   │   │   └── linear_gradient.png                                  │   │   │   └── linear_gradient.png
+│   │   └── rose.png                                                 │   │   └── rose.png
+│   └── Lorem.txt                                                    │   └── Lorem.txt
+└── ls_man_pages.txt                                                 └── ls_man_pages.txt
+```
+
+## Generating the Test Input
+
+The directory trees associated with the test are built through a series of
+commands that are executed by running `generate-test-input.sh`. We can run that
+script by running:
+
+```bash
+./generate-test-input.sh
+```
+
+## Credits
+
+I found the command used to generate `test.mp4` [here](https://vse-docs.readthedocs.io/video_editing/setup/creating-test-files.html).
+
+## The Aim of This Test
+
+The aim of this test is to serve as one of many tests that make sure
+`cmp-tree`:
+1. Correctly recurses into subdirectories
+2. Correctly identifies files that differ in substance but are the same size.
+3. Can correctly identify when directories are identical in every way except in
+   their modification time.
+4. Can work with files larger than 10 MB.
+
+## Expected Exit Codes
+
+Ran from `tests/013`:
+
+* `cmp-tree first/ second/` should exit with an exit code of 1.
+* `cmp-tree -d first/ second/` should exit with an exit code of 1.
+* `diff -qr first/ second/` should exit with an exit code of 1.
---
+++ tests/013/generate-test-input.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command fails
+set -e
+
+write_lorem_to_file () {
+	# {{{
+	printf "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
+		eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim \
+		ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
+		aliquip ex ea commodo consequat. Duis aute irure dolor in \
+		reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+		pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
+		culpa qui officia deserunt mollit anim id est laborum." > $1
+	# }}}
+}
+
+build_first () {
+	cur_wd=$(pwd)
+	mkdir -p first/
+	cd first/
+	man ls > ls_man_pages.txt
+
+	mkdir -p 02/
+	cd 02/
+	write_lorem_to_file Lorem.txt
+
+	mkdir -p 03/
+	cd 03/
+	magick rose: rose.png
+
+	mkdir -p 04/
+	cd 04/
+	magick -size 256x256 gradient: linear_gradient.png
+
+	mkdir -p 05/
+	cd 05/
+	ffmpeg -y -f lavfi -i testsrc=duration=10:size=1280x720:rate=30 test.mp4
+
+	mkdir -p 06/
+	cd 06/
+	ffmpeg -y -f lavfi -i gradients=duration=60:nb_colors=2:c0=green:c1=yellow:x0=500:x1=1500:y0=500:y1=1500:type=linear:speed=0.0125:size=2000x2000:rate=60 -pix_fmt yuv420p -g 1 -preset slow gradients.mov
+
+	# Return to the working directory where this function started
+	cd "$cur_wd"
+}
+
+build_second () {
+	cur_wd=$(pwd)
+	# Copy all the contents of the first/ to second/, preserving file
+	# metadata...
+	cp -arT first/ second/
+
+	# But then overwrite one of the files to be different
+	cd second/02/03/04/05/06/
+	ffmpeg -y -f lavfi -i gradients=duration=60:nb_colors=2:c0=yellow:c1=orange:x0=500:x1=1500:y0=500:y1=1500:type=linear:speed=0.0125:size=2000x2000:rate=60 -pix_fmt yuv420p -g 1 -preset slow gradients.mov
+
+	# Return to the working directory where this function started
+	cd "$cur_wd"
+}
+
+# I cannot guarantee that this won't cause issues with some file formats, but
+# it does seem to work with these .mov files although I have not thoroughly
+# tested that.
+pad_differing_file () {
+	size_first=$(stat -c%s "first/02/03/04/05/06/gradients.mov")
+	size_second=$(stat -c%s "second/02/03/04/05/06/gradients.mov")
+
+	if [[ "$size_first" -lt "$size_second" ]]; then
+		dd if=/dev/zero bs=1 count=$((size_second - size_first)) status=none >> "first/02/03/04/05/06/gradients.mov"
+	elif [[ "$size_first" -gt "$size_second" ]]; then
+		dd if=/dev/zero bs=1 count=$((size_first - size_second)) status=none >> "second/02/03/04/05/06/gradients.mov"
+	fi
+}
+
+# The body of the script, build the first directory tree, then the second
+build_first
+build_second
+# We also want to make sure the two differing files are of the exact same size
+# in terms of bytes, so we need to pad the smaller one.
+pad_differing_file
+
