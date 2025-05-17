2982a12e9ca8dec6cebd296387e7346ae7bebadf
JSpeedie <julianspeedie@gmail.com>
2025-05-17 11:56:16 -0400

    Added a new modification time test, renamed the first modification time test, revisited the integration test naming convention

--- rust/cmp-tree/tests/integration_tests.rs
+++ rust/cmp-tree/tests/integration_tests.rs
@@ -123,8 +123,38 @@ mod integration_tests {
         // differed? How did they differ?
     }
 
+    /* The naming convention for integration tests is to name each test like so:
+     *
+     *     it_[test-focus]_[test-focus-n]_[brief-description]
+     *
+     * where:
+     *     `it` stands for "integration test"
+     *     `test-focus` is a very short description of what condition we are testing the program
+     *         for correctness under.
+     *     `test-focus-n` represents the test id or number *within* the current `test-focus`. For
+     *         example, the third test related to modification time correctness might be called
+     *         `it_modification_times_003_...`
+     *     `brief-description` a short description of what exactly the inputs to this test
+     *         represent.
+     */
     #[test]
-    fn it011_2_level_trees_differing_modification_times() {
+    fn it_modification_times_001_two_reg_files_with_identical_mtimes() {
+        /* {{{ */
+        let mut conf = cmp_tree::default_config();
+        // By default, `cmp-tree` does not check that file metadata is identical as well.
+        // Enable metadata comparison.
+        conf.compare_metadata = true;
+        let first_dir = Path::new("../../tests/001/first");
+        let second_dir = Path::new("../../tests/001/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 0);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_modification_times_002_four_reg_files_with_differing_mtimes() {
+        /* {{{ */
         let mut conf = cmp_tree::default_config();
         // By default, `cmp-tree` does not check that file metadata is identical as well.
         // Enable metadata comparison.
@@ -134,7 +164,6 @@ mod integration_tests {
 
         let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
         assert_eq!(exit_code, 1);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
+        /* }}} */
     }
 }
