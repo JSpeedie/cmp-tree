29026351e3000796b2a4a80a59b254fe4f63e435
JSpeedie <julianspeedie@gmail.com>
2025-05-17 12:21:36 -0400

    Renamed integration tests to hopefully be clearer

--- rust/cmp-tree/tests/integration_tests.rs
+++ rust/cmp-tree/tests/integration_tests.rs
@@ -3,126 +3,6 @@ mod integration_tests {
 
     use cmp_tree;
 
-    #[test]
-    fn it001_1_level_trees_identical_regular_files() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/001/first");
-        let second_dir = Path::new("../../tests/001/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 0);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it002_1_level_trees_single_differing_regular_file() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/002/first");
-        let second_dir = Path::new("../../tests/002/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 1);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it003_2_level_trees_identical_regular_files() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/003/first");
-        let second_dir = Path::new("../../tests/003/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 0);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it004_2_level_trees_single_differing_regular_file() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/004/first");
-        let second_dir = Path::new("../../tests/004/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 1);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it005_1_level_trees_exclusively_differing_regular_files() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/005/first");
-        let second_dir = Path::new("../../tests/005/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 1);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it006_4_level_trees_identical_dirs() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/006/first");
-        let second_dir = Path::new("../../tests/006/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 0);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it007_4_level_trees_missing_a_subdir() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/007/first");
-        let second_dir = Path::new("../../tests/007/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 1);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it008_2_level_trees_identical_soft_link_that_points_to_identical_directories() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/008/first");
-        let second_dir = Path::new("../../tests/008/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 0);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it009_2_level_trees_differing_soft_links_that_point_to_differing_directories() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/009/first");
-        let second_dir = Path::new("../../tests/009/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 1);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
-    #[test]
-    fn it010_2_level_trees_differing_soft_links_that_point_to_identical_directories() {
-        let conf = cmp_tree::default_config();
-        let first_dir = Path::new("../../tests/010/first");
-        let second_dir = Path::new("../../tests/010/second");
-
-        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
-        assert_eq!(exit_code, 1);
-        // TODO: this should also check more detailed output. How many files differed? Which files
-        // differed? How did they differ?
-    }
-
     /* The naming convention for integration tests is to name each test like so:
      *
      *     it_[test-focus]_[test-focus-n]_[brief-description]
@@ -137,6 +17,127 @@ mod integration_tests {
      *     `brief-description` a short description of what exactly the inputs to this test
      *         represent.
      */
+
+    #[test]
+    fn it_general_001_identical_1l_with_regular_files() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/001/first");
+        let second_dir = Path::new("../../tests/001/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 0);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_general_002_differing_1l_single_differing_regular_file() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/002/first");
+        let second_dir = Path::new("../../tests/002/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 1);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_general_003_identical_2l_with_regular_files() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/003/first");
+        let second_dir = Path::new("../../tests/003/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 0);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_general_004_differing_2l_single_differing_regular_file() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/004/first");
+        let second_dir = Path::new("../../tests/004/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 1);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_general_005_differing_1l_two_differing_regular_files() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/005/first");
+        let second_dir = Path::new("../../tests/005/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 1);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_general_006_identical_4l_only_directories() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/006/first");
+        let second_dir = Path::new("../../tests/006/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 0);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_general_007_differing_4l_only_directories_missing_a_subdir() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/007/first");
+        let second_dir = Path::new("../../tests/007/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 1);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_soft_links_001_identical_soft_link_that_points_to_identical_directories() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/008/first");
+        let second_dir = Path::new("../../tests/008/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 0);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_soft_links_002_differing_soft_links_that_point_to_differing_directories() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/009/first");
+        let second_dir = Path::new("../../tests/009/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 1);
+        /* }}} */
+    }
+
+    #[test]
+    fn it_soft_links_003_differing_soft_links_that_point_to_identical_directories() {
+        /* {{{ */
+        let conf = cmp_tree::default_config();
+        let first_dir = Path::new("../../tests/010/first");
+        let second_dir = Path::new("../../tests/010/second");
+
+        let exit_code = cmp_tree::cmp_tree(&conf, &first_dir, &second_dir);
+        assert_eq!(exit_code, 1);
+        /* }}} */
+    }
+
     #[test]
     fn it_modification_times_001_two_reg_files_with_identical_mtimes() {
         /* {{{ */
@@ -153,7 +154,7 @@ mod integration_tests {
     }
 
     #[test]
-    fn it_modification_times_002_four_reg_files_with_differing_mtimes() {
+    fn it_modification_times_002_four_reg_files_all_with_differing_mtimes() {
         /* {{{ */
         let mut conf = cmp_tree::default_config();
         // By default, `cmp-tree` does not check that file metadata is identical as well.
