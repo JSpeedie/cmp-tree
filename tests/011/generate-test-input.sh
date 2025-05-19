51981349257c558669ff3e6ab51ce56167c96797
JSpeedie <julianspeedie@gmail.com>
2025-05-18 20:46:00 -0400

    Fixed inconsequential formatting error when creating Lorem text file

--- tests/011/generate-test-input.sh
+++ tests/011/generate-test-input.sh
@@ -5,13 +5,13 @@ set -e
 
 write_lorem_to_file () {
 	# {{{
-	printf "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
-		eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim \
-		ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
-		aliquip ex ea commodo consequat. Duis aute irure dolor in \
-		reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
-		pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
-		culpa qui officia deserunt mollit anim id est laborum." > $1
+	printf "\
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.\n" > $1
 	# }}}
 }
 
