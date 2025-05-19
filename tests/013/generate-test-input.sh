a204689b9eb26c681b765c7a0e976258392485f0
JSpeedie <julianspeedie@gmail.com>
2025-05-19 15:03:27 -0400

    Wrapped all paths in quotations for safety

--- tests/013/generate-test-input.sh
+++ tests/013/generate-test-input.sh
@@ -17,29 +17,29 @@ culpa qui officia deserunt mollit anim id est laborum.\n" > $1
 
 build_first () {
 	cur_wd=$(pwd)
-	mkdir -p first/
-	cd first/
-	man ls > ls_man_pages.txt
+	mkdir -p "first/"
+	cd "first/"
+	man ls > "ls_man_pages.txt"
 
 	mkdir -p 02/
 	cd 02/
-	write_lorem_to_file Lorem.txt
+	write_lorem_to_file "Lorem.txt"
 
 	mkdir -p 03/
 	cd 03/
-	magick rose: rose.png
+	magick rose: "rose.png"
 
 	mkdir -p 04/
 	cd 04/
-	magick -size 256x256 gradient: linear_gradient.png
+	magick -size 256x256 gradient: "linear_gradient.png"
 
 	mkdir -p 05/
 	cd 05/
-	ffmpeg -y -f lavfi -i testsrc=duration=10:size=1280x720:rate=30 test.mp4
+	ffmpeg -y -f lavfi -i testsrc=duration=10:size=1280x720:rate=30 "test.mp4"
 
 	mkdir -p 06/
 	cd 06/
-	ffmpeg -y -f lavfi -i gradients=duration=60:nb_colors=2:c0=green:c1=yellow:x0=500:x1=1500:y0=500:y1=1500:type=linear:speed=0.0125:size=2000x2000:rate=60 -pix_fmt yuv420p -g 1 -preset slow gradients.mov
+	ffmpeg -y -f lavfi -i gradients=duration=60:nb_colors=2:c0=green:c1=yellow:x0=500:x1=1500:y0=500:y1=1500:type=linear:speed=0.0125:size=2000x2000:rate=60 -pix_fmt yuv420p -g 1 -preset slow "gradients.mov"
 
 	# Return to the working directory where this function started
 	cd "$cur_wd"
@@ -49,11 +49,11 @@ build_second () {
 	cur_wd=$(pwd)
 	# Copy all the contents of the first/ to second/, preserving file
 	# metadata...
-	cp -arT first/ second/
+	cp -arT "first/" "second/"
 
 	# But then overwrite one of the files to be different
 	cd second/02/03/04/05/06/
-	ffmpeg -y -f lavfi -i gradients=duration=60:nb_colors=2:c0=yellow:c1=orange:x0=500:x1=1500:y0=500:y1=1500:type=linear:speed=0.0125:size=2000x2000:rate=60 -pix_fmt yuv420p -g 1 -preset slow gradients.mov
+	ffmpeg -y -f lavfi -i gradients=duration=60:nb_colors=2:c0=yellow:c1=orange:x0=500:x1=1500:y0=500:y1=1500:type=linear:speed=0.0125:size=2000x2000:rate=60 -pix_fmt yuv420p -g 1 -preset slow "gradients.mov"
 
 	# Return to the working directory where this function started
 	cd "$cur_wd"
@@ -73,7 +73,7 @@ pad_differing_file () {
 	fi
 }
 
-# The body of the script, build the first directory tree, then the second
+# The body of the script, build the first directory tree and the second
 build_first
 build_second
 # We also want to make sure the two differing files are of the exact same size
