3842fabd32848c8efd52f6a27af2adaaa24ae560
JSpeedie <julianspeedie@gmail.com>
2025-05-19 19:45:41 -0400

    Added test input 002, this time as a script which generates the two input directory trees in full

---
+++ tests/002/generate-test-input.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command fails
+set -e
+
+write_lorem_to_file () {
+	# {{{
+	printf "\
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.\n" > $1
+	# }}}
+}
+
+write_lorem_allcaps_to_file () {
+	# {{{
+	printf "\
+LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR
+INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS
+NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT.
+DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU
+FUGIAT NULLA PARIATUR. EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN
+CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.\n" > $1
+	# }}}
+}
+
+build_first () {
+	cur_wd=$(pwd)
+	mkdir -p "first/"
+	cd "first/"
+
+	man cmp > "cmp_man_pages.txt"
+	write_lorem_to_file "Lorem.txt"
+
+	# Return to the working directory where this function started
+	cd "$cur_wd"
+}
+
+build_second () {
+	cur_wd=$(pwd)
+	mkdir -p "second/"
+	cd "second/"
+
+	man cmp > "cmp_man_pages.txt"
+	write_lorem_allcaps_to_file "Lorem.txt"
+
+	# Return to the working directory where this function started
+	cd "$cur_wd"
+}
+
+# The body of the script, build the first input directory tree and the second
+build_first
+build_second
+
